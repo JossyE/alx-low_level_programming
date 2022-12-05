c7e41bed6cb90c60129b8936d8af0262e67a32ac
Josephine Effiong <105316743+JossyE@users.noreply.github.com>
2022-12-05 23:35:17 +0100

    Update 100-elf_header.c
    input_output

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -2,8 +2,8 @@
 
 
 /*
- * File: 100-elf_header.c
- */
+   * File: 100-elf_header.c
+   */
 /**
  * check_elf - Checks if a file is an ELF file.
  * @e_ident: A pointer to an array containing the ELF magic numbers.
@@ -170,7 +170,7 @@ void print_osabi(unsigned char *e_ident)
 void print_abi(unsigned char *e_ident)
 {
 	printf("  ABI Version:                       %d\n",
-			      e_ident[EI_ABIVERSION]);
+			       e_ident[EI_ABIVERSION]);
 }
 
 /**
